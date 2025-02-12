797bb5cc7ce5a3402a9b5f2d3ae0ef631cad295b
Javier Jiménez Fernández <javjimen@student.42madrid.com>
2025-02-12 19:11:56 +0100

    Rearrange main logic to avoid unnecessary return calls

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: javjimen <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 20:03:00 by javjimen          #+#    #+#             */
-/*   Updated: 2025/02/12 19:10:10 by javjimen         ###   ########.fr       */
+/*   Updated: 2025/02/12 19:10:56 by javjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,19 +19,17 @@ int	main(int argc, char **argv)
 	if (argc != 2)
 		error_handler(wrong_usage);
 	if (is_file_name_valid(argv[1]))
+	{
 		map = parse_map(argv[1]);
-	else
-	{
-		error_handler(map_name_error);
-		return (1);
-	}
-	if (!is_map_valid(map))
-	{
+		if (!is_map_valid(map))
+		{
+			free_map(map);
+			error_handler(invalid_map);
+		}
+		start_game(map);
 		free_map(map);
-		error_handler(invalid_map);
-		return (1);
 	}
-	start_game(map);
-	free_map(map);
+	else
+		error_handler(map_name_error);
 	return (0);
 }
